429101de6fbac7006a6cb5aec33a52ada39ad31f
Sean Clemmer <sczizzo@gmail.com>
2012-07-15 11:03:54 -0500

    Clean up, clean up, everybody clean up...

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,14 +1,14 @@
-function cycleImages(){
-      var $active = $('#portfolio_cycler .active');
-      var $next = ($active.next().length > 0) ? $active.next() : $('#portfolio_cycler img:first');
-      $next.css('z-index',2);//move the next image up the pile
-      $active.fadeOut(1500,function(){//fade out the top image
-	  $active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
-          $next.css('z-index',3).addClass('active');//make the next image the top one
-      });
-    }
+var cycleImages = function() {
+  var $active = $('#portfolio_cycler .active');
+  var $next   = ($active.next().length > 0) ? $active.next() : $('#portfolio_cycler img:first');
+
+  $next.css('z-index',2);                                   // Move the next image up the pile
+  $active.fadeOut(1500, function() {                        // Fade out the top image
+    $active.css('z-index',1).show().removeClass('active');  // Reset the z-index and unhide the image
+    $next.css('z-index',3).addClass('active');              // Make the next image the top one
+  });
+};
 
 $(document).ready(function(){
-// run every 7s
-setInterval('cycleImages()', 7000);
-})
+  setInterval('cycleImages()', 4000); // Run every 4 seconds
+});
