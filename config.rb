c7532cb188303a2189b5b4d099c84a3a685bfddf
Sean Clemmer <sczizzo@gmail.com>
2012-07-17 18:33:38 -0500

    Forgot about config...

--- config.rb
+++ config.rb
@@ -1,19 +1,19 @@
-# require 'sassy-buttons'
-# 
-# # Minify CSS
-# compass_config { |config| config.output_style = :compressed }
-# 
-# # Minify JavaScript
-# require 'uglifier'
-# set :js_compressor, Uglifier.new(:toplevel => true)
-# 
-# # Build options
-# set :css_dir, 'stylesheets'
-# set :js_dir, 'javascripts'
-# set :images_dir, 'images'
-# 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-#   activate :relative_assets
-# end
+require 'sassy-buttons'
+
+# Minify CSS
+compass_config { |config| config.output_style = :compressed }
+
+# Minify JavaScript
+require 'uglifier'
+set :js_compressor, Uglifier.new(:toplevel => true)
+
+# Build options
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :relative_assets
+end
