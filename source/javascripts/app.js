ea781779a60fbd643fbde9bb816f8309201162bc
Sean Clemmer <sczizzo@gmail.com>
2012-07-17 18:25:45 -0500

    New, sassy buttons; a little more style

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,14 +1,12 @@
 //= require 'jquery'
-//= require 'easing'
 
 var cycleImages = function() {
   var $active = $('#cycler .active');
-  var $next   = $active.next().length > 0 ? $active.next() : $('#cycler img:first');
+  var $next   = $active.next().length > 0 ? $active.next() : $('#cycler img:first-child');
   $active.removeClass('active');
   $next.addClass('active');
 };
 
 $(document).ready(function(){
-  $('#cycler img:first').addClass('active');
   setInterval('cycleImages()', 6000); // Run every 6 seconds
 });
